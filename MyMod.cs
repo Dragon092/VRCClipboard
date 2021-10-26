f243a88e53c81c57825051dd9a2ed02e801aa40a
Kevin Birke <mail@kevinbirke.de>
2021-10-26 03:23:14 +0200

    Initial commit

---
+++ MyMod.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using VRC;
+using VRC.Core;
+using VRChatUtilityKit.Utilities;
+using UIExpansionKit.API;
+using VRC.SDK3.Video.Components.AVPro;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using System.Reflection;
+using VRC.Udon;
+using VRC.SDK3.Video.Components;
+
+namespace VRCClipboard
+{
+    public class MyMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Video URL", ShowVideoMenu);
+
+            //ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Get Video", () => getVideoURL());
+            //ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Get Video Old", () => getVideoURLOld());
+
+            MelonLogger.Msg("Initialized!");
+        }
+
+        private void ShowVideoMenu()
+        {
+            VRCUrlInputField[] foundInputFields = GameObject.FindObjectsOfType<VRCUrlInputField>();
+
+            if(foundInputFields.Length == 0)
+            {
+                MelonLogger.Msg("No InputField found!");
+            }
+            else if (foundInputFields.Length == 1)
+            {
+                VRCUrlInputField inputField = foundInputFields[0];
+
+                VRCUrl url = new VRCUrl(Clipboard);
+                inputField.SetUrl(url);
+                inputField.onEndEdit.Invoke(inputField.text);
+            }
+            else
+            {
+                var menu = ExpansionKitApi.CreateCustomQuickMenuPage(LayoutDescription.QuickMenu4Columns);
+
+                foreach (VRCUrlInputField inputField in foundInputFields)
+                {
+                    MelonLogger.Msg("Found InputField: " + inputField.name);
+
+                    menu.AddSimpleButton(inputField.name, () => {
+                        VRCUrl url = new VRCUrl(Clipboard);
+                        inputField.SetUrl(url);
+                        inputField.onEndEdit.Invoke(inputField.text);
+                    });
+                }
+
+                menu.Show();
+            }
+        }
+
+            private static void loadVideo()
+        {
+            VRCUrlInputField[] foundInputFields = GameObject.FindObjectsOfType<VRCUrlInputField>();
+
+            foreach (VRCUrlInputField inputField in foundInputFields)
+            {
+                MelonLogger.Msg("Found InputField: "+inputField.name);
+
+                VRCUrl url = new VRCUrl(Clipboard);
+                inputField.SetUrl(url);
+                inputField.onEndEdit.Invoke(inputField.text);
+            }
+            }
+
+        
+        public static string Clipboard
+        {
+            get { return GUIUtility.systemCopyBuffer; }
+            set { GUIUtility.systemCopyBuffer = value; }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using VRCClipboard;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VRCClipboard")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VRCClipboard")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be717b40-e3df-4092-9f5e-48b39ae1648a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(MyMod), "VRCCipboard", "0.0.1", "Dragon", "https://github.com/Dragon092/VRCCipboard")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+[assembly: MelonAdditionalDependencies("VRChatUtilityKit", "UIExpansionKit")]
